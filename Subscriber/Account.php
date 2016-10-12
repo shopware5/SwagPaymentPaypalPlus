b65d7fc669c94c5117c668af5b9bc5f93573d50b
Niklas Gödde <ng@shopware.com>
2016-10-12 12:56:52 +0200

    PT-6763 - Fix a bug where CURL caused some invalid behaviours on the account page

--- Subscriber/Account.php
+++ Subscriber/Account.php
@@ -9,6 +9,9 @@
 
 namespace Shopware\SwagPaymentPaypalPlus\Subscriber;
 
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
+use Shopware\SwagPaymentPaypalPlus\Components\RestClient;
 use Shopware_Plugins_Frontend_SwagPaymentPaypalPlus_Bootstrap as Bootstrap;
 
 /**
@@ -60,12 +63,14 @@ class Account
 
         $newDescription = $this->bootstrap->Config()->get('paypalPlusDescription', '');
         $newAdditionalDescription = $this->bootstrap->Config()->get('paypalPlusAdditionalDescription', '');
-        if ($request->getActionName() == 'payment') {
+        $plusAvailable = $this->paypalPlusAvailable();
+
+        if ($plusAvailable && $request->getActionName() === 'payment') {
             //Fix payment description
             $payments = $action->View()->getAssign('sPaymentMeans');
             if (!empty($payments)) {
                 foreach ($payments as $key => $payment) {
-                    if ($payment['name'] == 'paypal') {
+                    if ($payment['name'] === 'paypal') {
                         $payments[$key]['description'] = $newDescription;
                         $payments[$key]['additionaldescription'] = $payment['additionaldescription'] . $newAdditionalDescription;
                         break;
@@ -76,10 +81,46 @@ class Account
         }
 
         $user = $view->getAssign('sUserData');
-        if (!empty($user['additional']['payment']['name']) && $user['additional']['payment']['name'] == 'paypal') {
+        if ($plusAvailable && !empty($user['additional']['payment']['name']) && $user['additional']['payment']['name'] === 'paypal') {
             $user['additional']['payment']['description'] = $newDescription;
             $user['additional']['payment']['additionaldescription'] = $newAdditionalDescription;
             $view->assign('sUserData', $user);
         }
+    }
+
+    /**
+     * Returns a value indicating whether or not the payment method "paypal plus" is available at the moment.
+     * It sends a test call to the api and if everything went fine, the method is available.
+     * Currently, the main purpose of this function is the SSL test.
+     *
+     * @return bool
+     */
+    private function paypalPlusAvailable()
+    {
+        /** @var RestClient $client */
+        $client = $this->bootstrap->get('paypal_plus.rest_client');
+
+        if($client === null) {
+            return false;
+        }
+
+        //Test the SSL Certificate that was provided by sending a simple test call to the PP API.
+        //This is required, because in this step no other request will be sent that may be validated.
+        //If this call fails, the payment method will not be modified and stay as paypal classic.
+        //The method can be extended with several other exceptions to generate a correct result. The existing ones
+        //do only check whether or not the SSL setup is correct.
+        try {
+            $client->get('identity/openidconnect/userinfo', array('schema' => 'openid'));
+        } catch (ClientException $ce) {
+            //It's okay, since this call is just a SSL test.
+        } catch (RequestException $re) {
+            //It's not okay, since it's an SSL exception
+            return false;
+        }
+
+        //Resets the rest client in order to use it later
+        $client = null;
+
+        return true;
     }
 }
